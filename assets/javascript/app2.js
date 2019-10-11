8002bc93fce56cff89278214bcea729b76fa7982
Joseph Young <joseph.young32@yahoo.com>
2019-10-11 17:09:43 -0400

    refactored logic, added similar artists cards dynamically

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,3 +1,4 @@
+//last.FM API info
 var apiKey = "a9934be7d65f534ea8c336db02853748";
 var queryURL = "https://ws.audioscrobbler.com/2.0/";
 
@@ -11,10 +12,13 @@ $(".btn-submit").on("click", function(event){
     var album = $("#album").val().trim();
 
     if (songTitle != "" && artist != ""){
+        //empty the previous results 
         $("#songTitleDisplay, #artistDisplay, #albumDisplay").empty();
+        //display a filler is the song doesnt have a picture
         $("#albumPic").attr("src", "http://lorempixel.com/200/200/technics");
-
+        //function to call the track
         getTrackInfo(songTitle, artist);
+        //function to pull up similar artist
         getSimilarArtist(artist);
     };
     
@@ -67,14 +71,18 @@ function getSimilarArtist(artist){
         console.log(response);
         console.log(response.similarartists.artist[0]);
 
-        $("#similarArtist1").text(response.similarartists.artist[0].name);
-        $("#artist1Pic").attr("src", response.similarartists.artist[0].image[1]["#text"]);
+        for(i = 0; i < 3; i++){
+            var similarArtistCard = `<div class="card col">
+            <img src="${response.similarartists.artist[i].image[1]["#text"]}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
+                <p class="card-text" id="similarArtist1Info"></p>
+            </div>
+        </div>`
 
-        $("#similarArtist2").text(response.similarartists.artist[1].name);
-        $("#artist2Pic").attr("src", response.similarartists.artist[1].image[1]["#text"]);
+        $("#similarArtist").append(similarArtistCard);
+        };
 
-        $("#similarArtist3").text(response.similarartists.artist[2].name);
-        $("#artist3Pic").attr("src", response.similarartists.artist[2].image[1]["#text"]);
     });
 
 };
