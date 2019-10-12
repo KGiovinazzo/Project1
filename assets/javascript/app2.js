adb22301e25d1ca39128109b87837b5d25fabf93
Joseph Young <joseph.young32@yahoo.com>
2019-10-11 20:22:06 -0400

    made API calls into functions on javascript page, cleaned up HTML and added info to page dynamically

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -6,88 +6,135 @@ var queryURL = "https://ws.audioscrobbler.com/2.0/";
 $(".btn-submit").on("click", function(event){
     event.preventDefault();
     console.log("click");
-
+    //variable to store input values
     var songTitle = $("#songTitle").val().trim();
     var artist = $("#artist").val().trim();
     var album = $("#album").val().trim();
 
     if (songTitle != "" && artist != ""){
-        //empty the previous results 
-        $("#songTitleDisplay, #artistDisplay, #albumDisplay").empty();
-        $("#similarArtist").empty();
-        //display a filler is the song doesnt have a picture
-        $("#albumPic").attr("src", "http://lorempixel.com/200/200/technics");
         //function to call the track
         getTrackInfo(songTitle, artist);
         //function to pull up similar artist
         getSimilarArtist(artist);
+    } else if (artist != "" && album != ""){
+        //function to pull up album info
+        getAlbumInfo(artist, album);
     };
-    
+
 });
 
-// queryURL = `${queryURL}?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
-
-//     getLfmData(artist);
-
-
-//     if (album != '') {
-//         queryURL = `https://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
-
-//         getLfmData(album);
-//     }
-    
-//         //queryURL = `https://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=${artist}&track=${songTitle}&api_key=${apiKey}&format=json`;
-//         //getLfmData(songTitle, album);
-//     }
-
-function getTrackInfo(songTitle, artist){
+//Last.FM API call put into functions
+function getTrackInfo(songTitle, artist) {
 
     trackQueryURL = `${queryURL}?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${songTitle}&format=json`;
 
     $.ajax({
-        url : trackQueryURL,
-        method : "GET",
-        dataType : "jsonp"
+        url: trackQueryURL,
+        method: "GET",
+        dataType: "jsonp"
     }).then(function(response){
-            console.log(response);
-            // console.log(response.track.album.image[2]["#text"]);
+        console.log(response);
 
-            $("#songTitleDisplay").text(response.track.name);
-            $("#artistDisplay").text(response.track.artist.name);
-            $("#albumDisplay").text(response.track.album.title);
-            $("#albumPic").attr("src", response.track.album.image[3]["#text"]);
-            
+        var displayTrack = `<div class="row justify-content-center">
+            <div class="card m-3 col flex-md-fill">
+                <div class="row no-gutters">
+                    <div class="col">
+                        <img src="${response.track.album.image[3]["#text"]}" class="card-img my-3">
+                    </div>
+                    <div class="col align-self-center">
+                        <div class="card-body">
+                            <h5 class="card-title text-center m-3">${response.track.name}</h5>
+                            <p class="card-text text-center">${response.track.artist.name}</p>
+                            <p class="card-text text-center">${response.track.album.title}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card-deck" id="similarArtist">
+            </div>
+        </div>`;
+
+        $("#displayArea").html(displayTrack);
+
     });
+
 };
 
 function getSimilarArtist(artist){
 
-    artistQueryURL = `${queryURL}?method=artist.getsimilar&artist=${artist}&api_key=${apiKey}&format=json`;
+    similarArtistQueryURL = `${queryURL}?method=artist.getsimilar&artist=${artist}&api_key=${apiKey}&format=json`;
 
     $.ajax({
-        url : artistQueryURL,
-        method : "GET",
-        dataType : "jsonp"
+        url: similarArtistQueryURL,
+        method: "GET",
+        dataType: "jsonp"
     }).then(function(response){
         console.log(response);
-        console.log(response.similarartists.artist[0]);
 
-        for(i = 0; i < 3; i++){
+        for (i = 0; i < 3; i++) {
             var similarArtistCard = `<div class="card col">
-            <img src="${response.similarartists.artist[i].image[1]["#text"]}" class="card-img-top">
+            <img src="${response.similarartists.artist[i].image[1]["#text"]}" class="card-img-top my-3">
             <div class="card-body">
                 <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
                 <p class="card-text" id="similarArtist1Info"></p>
             </div>
-        </div>`
+        </div>`;
 
+            
         $("#similarArtist").append(similarArtistCard);
+        
         };
 
     });
 
 };
 
+function getAlbumInfo(artist, album){
+    
+    albumQueryURL = `${queryURL}?method=album.getInfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
+
+    $.ajax({
+        url: albumQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+
+    });
+
+};
+
+function getArtistInfo(artist){
+
+    artistQueryURL = `${queryURL}?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
+
+    $.ajax({
+        url: artistQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+
+    });
+
+};
+
+function getSimilarTrack(songTitle, artist){
+
+    similarTrackQueryURL = `${queryURL}?method=track.getsimilar&artist=${artist}&track=${songTitle}&api_key=${apiKey}&format=json`;
+
+    $.ajax({
+        url: similarTrackQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+
+    });
+
+};
 
 // if (key === "artist") {
             //     $("#albumPic").attr("src", response.artist.image[3]["#text"]);
