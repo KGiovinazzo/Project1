985e4a72eb78a5a3f69c441717e193f60a7bcb10
Anish Patel <anishpatel39@gmail.com>
2019-10-12 11:30:03 -0400

    updated lyric.js

--- assets/javascript/lyric.js
+++ assets/javascript/lyric.js
@@ -1,32 +1,45 @@
 // Lyric API
-function gifSearch() 
+var queryURL = "http://api.musixmatch.com/ws/1.1/track.search?q_artist=justinbieber&page_size=3&page=1&s_track_rating=desc"
+var apiKey = "13cdf1d052803672f7fd64d4de39086c"
+
+
+//click function
+$(".btn-submit").on("click", function(event)
 {
-	var lyric = $(this).attr("data-movie");
-	var queryURL = ""
-// Api key - 13cdf1d052803672f7fd64d4de39086c
+    event.preventDefault();
+	console.log("click");
+	
+	var songTitle = $("#songTitle").val().trim();
+	var artist = $("#artist").val().trim();
+	var album = $("#album").val().trim();
+	console.log(songTitle);
+	console.log(artist);
+	console.log(album);
+})
 
 // Ajax
-	$.ajax(
-			
+	$.ajax(	
 {
 	url: queryURL,
-	method: "GET"
-}).done(function (response) 
+	method: "GET",
+	dataType: "jsonp"
 
-{
-	var results = response.data;
-	$("#lyrics").empty();
-    for (var i = 0; i < results.length; i++) 
-        {
-			var lyrics = $("<div>");
-			var p = $("<p>").text("Rating: " + results[i].rating);
-			var lyrics = $("<img>");
-			lyrics .attr("src", results[i].original_still.url);
-			lyrics .attr("data-still", results[i].original_still.url);
-			lyrics .attr("data-state", "still");
-			lyrics .append(lyrics );
-			$("#lyrics").append(gifDiv);
-		}
+}).then(function (response){
+console.log(response);
 
-	});
-};
+function titleSearch()
+
+	var titleSearch
+
+
+
+
+})
+
+
+
+
+
+
+
+	
