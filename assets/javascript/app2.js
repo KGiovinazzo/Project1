6cb69b49a368b74b88c6896dddc4673b80c9d5f7
Joseph Young <joseph.young32@yahoo.com>
2019-10-12 21:51:15 -0400

    made click events for artist info and similar artist button

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,32 +1,46 @@
 //last.FM API info
 var apiKey = "a9934be7d65f534ea8c336db02853748";
 var queryURL = "https://ws.audioscrobbler.com/2.0/";
+var songTitle;
+var artist;
+var album;
 
 //make a click function for our form submit button
 $(".btn-submit").on("click", function(event){
     event.preventDefault();
     console.log("click");
     //variable to store input values
-    var songTitle = $("#songTitle").val().trim();
-    var artist = $("#artist").val().trim();
-    var album = $("#album").val().trim();
+    songTitle = $("#songTitle").val().trim();
+    artist = $("#artist").val().trim();
+    album = $("#album").val().trim();
 
     if (songTitle != "" && artist != ""){
         //function to call the track
         getTrackInfo(songTitle, artist);
-        //function to pull up similar artist
-        getSimilarArtist(artist);
     } else if (artist != "" && album != ""){
         //function to pull up album info
         getAlbumInfo(artist, album);
     };
 
 });
+//click event for artist info button
+$("#artistInfo").on("click", function(event){
+    console.log(artist);
+    $("#displayArea").empty();
+    getArtistInfo(artist);
+});
+//click event for similar artists button
+$("#similarArtist").on("click", function(event){
+    console.log(artist);
+    $("#displayArea").empty();
+    $("#displayArea").html(`<div class="row d-flex justify-content-around" id="displaySimilarArtist"></div>`);
+    getSimilarArtist(artist);
+});
 
 //Last.FM API call put into functions
 function getTrackInfo(songTitle, artist) {
 
-    trackQueryURL = `${queryURL}?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${songTitle}&format=json`;
+    var trackQueryURL = `${queryURL}?method=track.getInfo&api_key=${apiKey}&artist=${artist}&track=${songTitle}&format=json`;
 
     $.ajax({
         url: trackQueryURL,
@@ -35,8 +49,7 @@ function getTrackInfo(songTitle, artist) {
     }).then(function(response){
         console.log(response);
 
-        var displayTrack = `<div class="row justify-content-center">
-            <div id = "display" class="card m-3 col flex-md-fill">
+        var displayTrack = `<div id = "display" class="card m-3 col flex-md-fill">
                 <div class="row no-gutters">
                     <div class="col">
                         <img src="${response.track.album.image[3]["#text"]}" class="card-img my-3">
@@ -50,10 +63,6 @@ function getTrackInfo(songTitle, artist) {
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="card-deck" id="similarArtist">
-            </div>
         </div>`;
 
         $("#displayArea").html(displayTrack);
@@ -64,7 +73,7 @@ function getTrackInfo(songTitle, artist) {
 
 function getSimilarArtist(artist){
 
-    similarArtistQueryURL = `${queryURL}?method=artist.getsimilar&artist=${artist}&api_key=${apiKey}&format=json`;
+    var similarArtistQueryURL = `${queryURL}?method=artist.getsimilar&artist=${artist}&api_key=${apiKey}&format=json`;
 
     $.ajax({
         url: similarArtistQueryURL,
@@ -73,16 +82,18 @@ function getSimilarArtist(artist){
     }).then(function(response){
         console.log(response);
 
-        for (i = 0; i < 3; i++) {
-            var similarArtistCard = `<div class="card col" id= "cardCenter">
-            <img src="${response.similarartists.artist[i].image[1]["#text"]}" class="card-img-top my-3">
-            <div class="card-body" id="cardBody">
-                <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
-                <p class="card-text" id="similarArtist1Info"></p>
-            </div>
-        </div>`;
- 
-        $("#similarArtist").append(similarArtistCard);
+        for (i = 0; i < 6; i++){
+            var similarArtistCard = `<div class="col-sm">
+                <div class="card m-3">
+                    <h5 class="card-header text-center">${i + 1}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>`;
+        
+        $("#displaySimilarArtist").append(similarArtistCard);
         
         };
 
@@ -92,7 +103,7 @@ function getSimilarArtist(artist){
 
 function getAlbumInfo(artist, album){
     
-    albumQueryURL = `${queryURL}?method=album.getInfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
+    var albumQueryURL = `${queryURL}?method=album.getInfo&api_key=${apiKey}&artist=${artist}&album=${album}&format=json`;
 
     $.ajax({
         url: albumQueryURL,
@@ -107,7 +118,7 @@ function getAlbumInfo(artist, album){
 
 function getArtistInfo(artist){
 
-    artistQueryURL = `${queryURL}?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
+    var artistQueryURL = `${queryURL}?method=artist.getinfo&artist=${artist}&api_key=${apiKey}&format=json`;
 
     $.ajax({
         url: artistQueryURL,
@@ -115,6 +126,16 @@ function getArtistInfo(artist){
         dataType: "jsonp"
     }).then(function(response){
         console.log(response);
+        
+        var artistDisplay = `<div class="card m-5">
+            <div class="card-header text-center">Summary</div>
+            <div class="card-body">
+                <h5 class="card-title text-center m-3">${response.artist.name}</h5>
+                <p class="card-text m-5">${response.artist.bio.summary}</p>
+            </div>
+        </div>`;
+
+        $("#displayArea").html(artistDisplay);
 
     });
 
@@ -122,7 +143,7 @@ function getArtistInfo(artist){
 
 function getSimilarTrack(songTitle, artist){
 
-    similarTrackQueryURL = `${queryURL}?method=track.getsimilar&artist=${artist}&track=${songTitle}&api_key=${apiKey}&format=json`;
+    var similarTrackQueryURL = `${queryURL}?method=track.getsimilar&artist=${artist}&track=${songTitle}&api_key=${apiKey}&format=json`;
 
     $.ajax({
         url: similarTrackQueryURL,
