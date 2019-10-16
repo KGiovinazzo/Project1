99107448ee240800ebffa181973f933b0a854944
Joseph Young <joseph.young32@yahoo.com>
2019-10-15 21:48:09 -0400

    got basic search functionality working on all input fields

--- assets/javascript/last-fm-api.js
+++ assets/javascript/last-fm-api.js
@@ -14,14 +14,19 @@ $(".btn-search").on("click", function(event){
     artist = $("#artist").val().trim();
     album = $("#album").val().trim();
 
-    if(artist != ""){
+    if(songTitle != ""){
+        searchCard();
+        searchTrack(songTitle);
+
+        return $("#songTitle").val("");
+    }else if(artist != ""){
         //function to generate buttons for artist only input
-        albumSearch();
+        searchCard();
         searchArtist(artist);
         
         return $("#artist").val("");
     } else if(album != ""){
-        albumSearch();
+        searchCard();
         searchAlbum(album);
         
         return $("#album").val("");
@@ -43,12 +48,12 @@ $(".btn-submit").on("click", function(event){
         return $("#songTitle,#artist").val("");
     } else if(artist != "" && album != ""){
         //function to pull up album info
-        getAlbumInfo(artist, album);
+        //getAlbumInfo(artist, album);
 
         return $("#artist,#album").val("");
     } else if(artist != ""){
         //function to generate buttons for artist only input
-        mainDisplayCard();
+        mainArtistCard();
         getArtistInfo(artist);
 
         return $("#artist").val("");
@@ -56,7 +61,6 @@ $(".btn-submit").on("click", function(event){
         albumSearch();
         searchAlbum(album);
         
-
         return $("#album").val("");
     };
 });
@@ -85,7 +89,7 @@ $(document).on("click", "#topTracksBtn", function(event){
     getTopTracks(artist);
 });
 //function to make main display card when submit button is clicked
-function mainDisplayCard(){
+function mainArtistCard(){
     var mainCard = `<div class="card text-center m-5 cbod lettering">
         <div class="card-header cbod">
             <ul class="nav nav-tabs card-header-tabs cbod justify-content-center">
@@ -113,27 +117,60 @@ function mainDisplayCard(){
 
 };
 //function to make album search card
-function albumSearch(){
-    var albumSearchCard = `<div class="card text-center m-5 cbod lettering">
+function searchCard(){
+    var searchCard = `<div class="card text-center m-5 cbod lettering">
         <div class="card-body cbod">
             <div class="justify-content-center cbod" id="displayArea">
             </div>
         </div>    
     </div>`;
 
-    $("#mainDisplay").html(albumSearchCard);
+    $("#mainDisplay").html(searchCard);
 };
-//function to generate button when only artist is input
-function artistInputButtons(){
-    var artistButtonSet = `<button type="button" class="btn btn-primary" id="artistInfoBtn">Artist Info</button>
-    <button type="button" class="btn btn-primary" id="similarArtistBtn">Similar Artists</button>
-    <button type="button" class="btn btn-primary" id="topAlbumsBtn">Top Albums</button>
-    <button type="button" class="btn btn-primary"
-    id="topTracksBtn">Top Tracks</button>`;
 
-    $("#buttonRow").html(artistButtonSet);
-};
+
+
 //Last.FM API call put into functions
+
+
+
+//function to search track
+function searchTrack(songTitle){
+    var searchTrackQueryURL = `${lastFmQueryURL}?method=track.search&track=${songTitle}&api_key=${lastFmApiKey}&format=json`;
+
+    $.ajax({
+        url: searchTrackQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+        var listSearchTrack = `<div class="card flex-fill m-5 cbod lettering">
+            <h5 class="card-header text-center fontWhite">Possible songs titled: "${songTitle}"</h5>
+            <div class="card-body">
+            <div class="card-columns" id="searchTrack"></div>
+        </div>
+    </div>`;
+
+    $("#displayArea").html(listSearchTrack);
+
+    for(i = 0; i < 9; i++){
+        var eachTrackSearchCard = `<div class="col-sm">
+            <div class="card m-3">
+                <h5 class="card-header text-center">${i + 1}</h5>
+                <div class="card-body">
+                    <h5 class="card-title text-center">Title: "${response.results.trackmatches.track[i].name}"</h5>
+                    <p class="card-text text-center">Artist: "${response.results.trackmatches.track[i].artist}"</p>
+                    <a href="${response.results.trackmatches.track[i].url}">Learn more</a>
+                </div>
+            </div>
+        </div>`;
+
+        $("#searchTrack").append(eachTrackSearchCard);
+        
+        };
+    });
+};
+
 //function to search artist
 function searchArtist(artist){
     var searchArtistQueryURL = `${lastFmQueryURL}?method=artist.search&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
@@ -169,133 +206,7 @@ function searchArtist(artist){
         };
     });
 };
-//function to get artist info
-function getArtistInfo(artist){
-    var artistQueryURL = `${lastFmQueryURL}?method=artist.getinfo&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
 
-    $.ajax({
-        url: artistQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        var artistDisplay = `<div class="card m-5 cbod lettering">
-            <h5 class="card-header text-center fontWhite lettering">${response.artist.name}</h5>
-            <div class="card-body fontWhite lettering">
-                <h5 class="card-title text-center m-3 lettering">Summary</h5>
-                <p class="card-text m-5 fontWhite">${response.artist.bio.summary}</p>
-            </div>
-        </div>`;
-
-        $("#displayArea").html(artistDisplay);
-
-    });
-};
-//function to get similar artist info
-function getSimilarArtist(artist){
-    var similarArtistQueryURL = `${lastFmQueryURL}?method=artist.getsimilar&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
-
-    $.ajax({
-        url: similarArtistQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        console.log(response);
-        console.log(response.similarartists["@attr"].artist);
-        var similarArtistHeadCard = `<div class="card flex-fill m-5 cbod lettering">
-            <h5 class="card-header text-center fontWhite">${response.similarartists["@attr"].artist}</h5>
-            <div class="card-body">
-            <div class="card-columns" id="displaySimilarArtist"></div>
-            </div>
-        </div>`;
-
-        $("#displayArea").html(similarArtistHeadCard);
-
-        for(i = 0; i < 6; i++){
-            var similarArtistCard = `<div class="col-sm">
-                <div class="card m-3">
-                    <h5 class="card-header text-center">${i + 1}</h5>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
-                        <p class="card-text"></p>
-                    </div>
-                </div>
-            </div>`;
-        
-        $("#displaySimilarArtist").append(similarArtistCard);
-        
-        };
-
-    });
-};
-//function to grab top albums
-function getTopAlbums(artist){
-    var topAlbumQueryURL = `${lastFmQueryURL}?method=artist.gettopalbums&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
-
-    $.ajax({
-        url: topAlbumQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        var topAlbumCard = `<div class="card flex-fill m-5 cbod lettering">
-            <h5 class="card-header text-center fontWhite">${response.topalbums["@attr"].artist}</h5>
-            <div class="card-body">
-            <div class="card-columns" id="topAlbums"></div>
-            </div>
-        </div>`;
-
-        $("#displayArea").html(topAlbumCard);
-
-        for(i = 0; i < 6; i++){
-            var eachAlbumCard = `<div class="col-sm">
-                <div class="card m-3">
-                    <h5 class="card-header text-center">${i + 1}</h5>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">${response.topalbums.album[i].name}</h5>
-                        <a href="${response.topalbums.album[i].url}">Learn more</a>
-                    </div>
-                </div>
-            </div>`;
-
-            $("#topAlbums").append(eachAlbumCard);
-        };
-
-    });
-};
-
-//function get top tracks
-function getTopTracks(artist){
-    var topTrackQueryURL = `${lastFmQueryURL}?method=artist.gettoptracks&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
-
-    $.ajax({
-        url: topTrackQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        var topTrackCard = `<div class="card flex-fill m-5 cbod lettering">
-            <h5 class="card-header text-center fontWhite">${response.toptracks["@attr"].artist}</h5>
-            <div class="card-body">
-            <div class="card-columns" id="topTracks"></div>
-            </div>
-        </div>`;
-
-        $("#displayArea").html(topTrackCard);
-
-        for(i = 0; i < 6; i++){
-            var eachTrackCard = `<div class="col-sm">
-                <div class="card m-3">
-                    <h5 class="card-header text-center">${i + 1}</h5>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">${response.toptracks.track[i].name}</h5>
-                        <a href="${response.toptracks.track[i].url}">Learn more</a>
-                    </div>
-                </div>
-            </div>`;
-
-            $("#topTracks").append(eachTrackCard);
-        };
-
-    });
-};
 //function to search for album
 function searchAlbum(album){
     var searchAlbumQueryURL = `${lastFmQueryURL}?method=album.search&album=${album}&api_key=${lastFmApiKey}&format=json`;
@@ -333,31 +244,11 @@ function searchAlbum(album){
         };
     });
 };
-//function to get album info
-function getAlbumInfo(artist, album){
-    var albumQueryURL = `${lastFmQueryURL}?method=album.getInfo&api_key=${lastFmApiKey}&artist=${artist}&album=${album}&format=json`;
 
-    $.ajax({
-        url: albumQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        console.log(response);
 
-    });
-};
-//function to search track
-function searchTrack(songTitle){
-    var searchTrackQueryURL = `${lastFmQueryURL}?method=track.search&track=${songTitle}&api_key=${lastFmApiKey}&format=json`;
 
-    $.ajax({
-        url: searchTrackQueryURL,
-        method: "GET",
-        dataType: "jsonp"
-    }).then(function(response){
-        console.log(response);
-    });
-};
+
+
 //function to pull up track info
 function getTrackInfo(songTitle, artist) {
     var trackQueryURL = `${lastFmQueryURL}?method=track.getInfo&api_key=${lastFmApiKey}&artist=${artist}&track=${songTitle}&format=json`;
@@ -387,6 +278,47 @@ function getTrackInfo(songTitle, artist) {
 
     });
 };
+
+//function to get artist info
+function getArtistInfo(artist){
+    var artistQueryURL = `${lastFmQueryURL}?method=artist.getinfo&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
+
+    $.ajax({
+        url: artistQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        var artistDisplay = `<div class="card m-5 cbod lettering">
+            <h5 class="card-header text-center fontWhite lettering">${response.artist.name}</h5>
+            <div class="card-body fontWhite lettering">
+                <h5 class="card-title text-center m-3 lettering">Summary</h5>
+                <p class="card-text m-5 fontWhite">${response.artist.bio.summary}</p>
+            </div>
+        </div>`;
+
+        $("#displayArea").html(artistDisplay);
+
+    });
+};
+
+//function to get album info
+function getAlbumInfo(artist, album){
+    var albumQueryURL = `${lastFmQueryURL}?method=album.getInfo&api_key=${lastFmApiKey}&artist=${artist}&album=${album}&format=json`;
+
+    $.ajax({
+        url: albumQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+
+    });
+};
+
+
+
+
+
 //function to get similar track
 function getSimilarTrack(songTitle, artist){
 
@@ -398,6 +330,117 @@ function getSimilarTrack(songTitle, artist){
         dataType: "jsonp"
     }).then(function(response){
         console.log(response);
+
+    });
+};
+
+//function to get similar artist info
+function getSimilarArtist(artist){
+    var similarArtistQueryURL = `${lastFmQueryURL}?method=artist.getsimilar&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
+
+    $.ajax({
+        url: similarArtistQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        console.log(response);
+        console.log(response.similarartists["@attr"].artist);
+        var similarArtistHeadCard = `<div class="card flex-fill m-5 cbod lettering">
+            <h5 class="card-header text-center fontWhite">${response.similarartists["@attr"].artist}</h5>
+            <div class="card-body">
+            <div class="card-columns" id="displaySimilarArtist"></div>
+            </div>
+        </div>`;
+
+        $("#displayArea").html(similarArtistHeadCard);
+
+        for(i = 0; i < 6; i++){
+            var similarArtistCard = `<div class="col-sm">
+                <div class="card m-3">
+                    <h5 class="card-header text-center">${i + 1}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${response.similarartists.artist[i].name}</h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>`;
+        
+        $("#displaySimilarArtist").append(similarArtistCard);
+        
+        };
+    });
+};
+
+
+
+
+
+//function get top tracks
+function getTopTracks(artist){
+    var topTrackQueryURL = `${lastFmQueryURL}?method=artist.gettoptracks&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
+
+    $.ajax({
+        url: topTrackQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        var topTrackCard = `<div class="card flex-fill m-5 cbod lettering">
+            <h5 class="card-header text-center fontWhite">${response.toptracks["@attr"].artist}</h5>
+            <div class="card-body">
+            <div class="card-columns" id="topTracks"></div>
+            </div>
+        </div>`;
+
+        $("#displayArea").html(topTrackCard);
+
+        for(i = 0; i < 6; i++){
+            var eachTrackCard = `<div class="col-sm">
+                <div class="card m-3">
+                    <h5 class="card-header text-center">${i + 1}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${response.toptracks.track[i].name}</h5>
+                        <a href="${response.toptracks.track[i].url}">Learn more</a>
+                    </div>
+                </div>
+            </div>`;
+
+            $("#topTracks").append(eachTrackCard);
+        };
+
+    });
+};
+
+//function to grab top albums
+function getTopAlbums(artist){
+    var topAlbumQueryURL = `${lastFmQueryURL}?method=artist.gettopalbums&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
+
+    $.ajax({
+        url: topAlbumQueryURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function(response){
+        var topAlbumCard = `<div class="card flex-fill m-5 cbod lettering">
+            <h5 class="card-header text-center fontWhite">${response.topalbums["@attr"].artist}</h5>
+            <div class="card-body">
+            <div class="card-columns" id="topAlbums"></div>
+            </div>
+        </div>`;
+
+        $("#displayArea").html(topAlbumCard);
+
+        for(i = 0; i < 6; i++){
+            var eachAlbumCard = `<div class="col-sm">
+                <div class="card m-3">
+                    <h5 class="card-header text-center">${i + 1}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${response.topalbums.album[i].name}</h5>
+                        <a href="${response.topalbums.album[i].url}">Learn more</a>
+                    </div>
+                </div>
+            </div>`;
+
+            $("#topAlbums").append(eachAlbumCard);
+        };
 
     });
 };
